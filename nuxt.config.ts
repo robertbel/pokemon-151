e48bde44268a65f52d809384ed828380a7d0bbc6
robertbel <r.bellekom@gmail.com>
2023-07-14 07:51:10 +0200

    Starting hero section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
-  css: ["@/assets/css/reset.css"],
+  css: [
+    "@/assets/css/reset.css",
+    "@/assets/css/typography.css",
+    "@/assets/css/spacing.css",
+    "@/assets/css/colors.css",
+    "@/assets/css/base.css",
+  ],
   nitro: {
     preset: "vercel-edge",
   },
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="main">
+    <div class="wrapper">
+      <section class="section">
+        <div class="section-content">
+          <div>Ontdek de glans van het verleden!</div>
+          <h1>Pokémon Scarlet & Violet 151</h1>
+          <p>
+            Verlies jezelf opnieuw in de wereld van Kanto met 'Pokémon Scarlet &
+            Violet 151'. Beleef je favoriete Pokémon op een nieuwe en spannende
+            manier. Ga het avontuur aan, en verzamel ze allemaal!
+          </p>
+          <div class="actions">
+            <button class="primary">Pre-order bij Bol.com!</button>
+            <button class="secondary">Ontdek paketten</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.wrapper {
+  max-width: 980px;
+  margin-inline: auto;
+}
+
+.section {
+  display: grid;
+  align-items: center;
+  min-height: 100vh;
+  padding-inline: var(--space-l-xl);
+  color: var(--font-base-color);
+}
+
+.section-content {
+  max-width: 37.5rem;
+}
+</style>
