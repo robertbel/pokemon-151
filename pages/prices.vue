2ded6a464082d7676a4fb0c93dc597b40787a477
robertbel <r.bellekom@gmail.com>
2023-07-24 10:35:17 +0200

    Some scrape testing

---
+++ pages/prices.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Prices</h1>
+  <div v-for="(price, url) in prices" :key="url">
+    <p>{{ url }}: {{ price }}</p>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+
+let prices = ref({});
+
+const fetchPrices = async () => {
+  try {
+    const { data } = await axios.get("http://localhost:3000/api/scrape");
+    prices.value = data;
+  } catch (error) {
+    console.error("Error fetching prices: ", error);
+  }
+};
+
+fetchPrices();
+</script>
