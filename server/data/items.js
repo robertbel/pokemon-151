e8fcff27f3b83272cab1ec910f210142ecd228e0
robertbel <r.bellekom@gmail.com>
2023-08-04 12:54:13 +0200

    Again

--- server/data/items.js
+++ server/data/items.js
@@ -1,41 +1,45 @@
 import prisma from "./prisma";
 
 export async function getItems() {
-  // Find the latest pull date
-  const latestPull = await prisma.products.findFirst({
-    orderBy: {
-      pulled_date: "desc",
-    },
-    select: {
-      pulled_date: true,
-    },
-  });
+  try {
+    // Find the latest pull date
+    const latestPull = await prisma.products.findFirst({
+      orderBy: {
+        pulled_date: "desc",
+      },
+      select: {
+        pulled_date: true,
+      },
+    });
 
-  const latestPullDate = latestPull.pulled_date;
+    const latestPullDate = latestPull.pulled_date;
 
-  // Use the latest pull date to find products
-  let products = await prisma.products.findMany({
-    where: {
-      AND: [
-        {
-          product_categories: {
-            set_id: 1,
+    // Use the latest pull date to find products
+    let products = await prisma.products.findMany({
+      where: {
+        AND: [
+          {
+            product_categories: {
+              set_id: 1,
+            },
+          },
+          {
+            pulled_date: latestPullDate,
+          },
+        ],
+      },
+      include: {
+        product_categories: true,
+        webshops: {
+          select: {
+            webshop_name: true,
           },
         },
-        {
-          pulled_date: latestPullDate,
-        },
-      ],
-    },
-    include: {
-      product_categories: true,
-      webshops: {
-        select: {
-          webshop_name: true,
-        },
       },
-    },
-  });
+    });
 
-  return products;
+    return products;
+  } catch (error) {
+    console.error("NONONO ", error);
+  }
 }
