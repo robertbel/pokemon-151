759e973d8b0d73c2fc36370a9dd79f91c17af3ed
robertbel <r.bellekom@gmail.com>
2023-08-04 11:22:09 +0200

    possible production fix

--- server/api/products.get.js
+++ server/api/products.get.js
@@ -1,41 +1,5 @@
-// import prisma from "../data/prisma";
+import { getItems } from "../data/items";
 
-// export default defineEventHandler(async () => {
-//   // Find the latest pull date
-//   const latestPull = await prisma.products.findFirst({
-//     orderBy: {
-//       pulled_date: "desc",
-//     },
-//     select: {
-//       pulled_date: true,
-//     },
-//   });
-
-//   const latestPullDate = latestPull.pulled_date;
-
-//   // Use the latest pull date to find products
-//   let products = await prisma.products.findMany({
-//     where: {
-//       AND: [
-//         {
-//           product_categories: {
-//             set_id: 1,
-//           },
-//         },
-//         {
-//           pulled_date: latestPullDate,
-//         },
-//       ],
-//     },
-//     include: {
-//       product_categories: true,
-//       webshops: {
-//         select: {
-//           webshop_name: true,
-//         },
-//       },
-//     },
-//   });
-
-//   return products;
-// });
+export default defineEventHandler(async () => {
+  return await getItems();
+});
